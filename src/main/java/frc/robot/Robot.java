26278c0445d0a967f77a29ca933ada05b455c172
Jeremiah Johnson <mrtidy@gmail.com>
2019-01-18 21:13:06 -0500

    updates based on testing today
    Removed the feed forward code and replaced with simple math.
    
    Added camera server to see how well USB camera comes thru.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,9 +1,10 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 
@@ -11,9 +12,15 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * Simple sample arcade drive type robot based on WPI and Phoenix samples.
  */
 public class Robot extends TimedRobot {
+    private Joystick joystick = new Joystick(0);
     private VictorSPX leftMotor = new VictorSPX(1);
     private VictorSPX rightMotor = new VictorSPX(2);
-    private Joystick joystick = new Joystick(0);
+
+    @Override
+    public void robotInit() {
+        UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+        camera.setResolution(320, 240);
+    }
 
     @Override
     public void teleopInit() {
@@ -26,11 +33,15 @@ public class Robot extends TimedRobot {
     public void teleopPeriodic() {
         // joystick Y is negative when pushed away
         double forward = filterDeadband(-1 * joystick.getY());
-        double turn = filterDeadband(joystick.getTwist());
-        leftMotor.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, turn);
-        rightMotor.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, -turn);
+        double side = filterDeadband(joystick.getX());
+        leftMotor.set(ControlMode.PercentOutput, (forward - side) / 2);
+        rightMotor.set(ControlMode.PercentOutput, (forward + side) / 2);
     }
 
+    /**
+     * This came from CTRE example - I like it because the joystick will
+     * often cause the bot to creep when it should just be sitting.
+     */
     private double filterDeadband(double value) {
         if (value >= 0.05) {
             return value;
